d938b3c43cebb1752fe71d20467ad6058c0effe8
kiran6055 <133787313+kiran6055@users.noreply.github.com>
2024-07-10 00:05:26 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,8 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull abhishekf5/simple-python-flask-app
+docker pull 358308582535.dkr.ecr.ap-south-1.amazonaws.com/test:latest
+
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app
+docker run -d -p 5000:5000 358308582535.dkr.ecr.ap-south-1.amazonaws.com/test:latest
